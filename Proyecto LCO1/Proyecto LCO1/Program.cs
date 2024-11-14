cc6aa783b4c586076cf32ef6549f6cb73a955e5d
Christian Jhonatan Martinez Chavez <cm8915620@gmail.com>
2024-11-14 12:19:32 -0500

    Segunda actualización del Proyecto

--- Proyecto LCO1/Proyecto LCO1/Program.cs
+++ Proyecto LCO1/Proyecto LCO1/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mi nombre es Christian");
 Console.WriteLine("Mi correo es :cm8915620@gmail.com");
+Console.WriteLine("Mi numero de celular es:910022807");
